98ceb3b052122d4925acb2804d108557d2d69f59
JGOliveira01 <a18816@alunos.ipca.pt>
2023-05-03 17:38:29 +0100

    SpaceShooter Fully Functional

---
+++ SpaceShooter/SpaceShooter/Sprites/Explosion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using SpaceShooter.Managers;
+using SpaceShooter.Models;
+using SpaceShooter.Models;
+
+namespace SpaceShooter.Sprites
+{
+    public class Explosion : Sprite
+    {
+        private float _timer = 0f;
+
+        public Explosion(Dictionary<string, Animation> animations) : base(animations)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _animationManager.Update(gameTime);
+
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > _animationManager.CurrentAnimation.FrameCount * _animationManager.CurrentAnimation.FrameSpeed)
+                IsRemoved = true;
+        }
+    }
+}
